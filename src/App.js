4792aff8b363da8a242da3fe4b0007dab04b28fc
sagartare <sagartare2014@gmail.com>
2023-11-05 20:43:15 +0530

    Components created as per mentioned in hackathon requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { WhatsHot } from './components/WhatsHot';
+import { NewReleases } from './components/NewReleases';
+import { HeaderHome } from './components/HeaderHome';
+import { FooterHome } from './components/FooterHome';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <BrowserRouter>
+        <HeaderHome/>
+    <Routes>
+      <Route index element={<WhatsHot />} />
+        <Route path="new" element={<NewReleases />} />
+        <Route path="hot" element={<WhatsHot />} />
+    </Routes>
+  </BrowserRouter>
+  <FooterHome/>
     </div>
   );
 }
